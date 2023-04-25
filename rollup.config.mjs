fe02c0459146fc4d61fb12a9801dc4dc6f1106b5
tmadaj <madaj.tomas@gmail.com>
2023-04-25 17:34:38 +0200

    chore: tuned setup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
-import replace from 'rollup-plugin-replace';
+import dotenv from 'dotenv';
 import serve from 'rollup-plugin-serve';
 import typescript from 'rollup-plugin-typescript2';
 
@@ -10,6 +11,8 @@ import packageJson from './package.json' assert { type: 'json' };
 const configDev = process.argv.includes('--configDev');
 const configProd = process.argv.includes('--configProd');
 const configServe = process.argv.includes('--configServe');
+
+dotenv.config();
 
 export default [
   {
@@ -22,17 +25,18 @@ export default [
       },
     ],
     plugins: [
+      replace({
+        preventAssignment: true,
+        'process.env.GIPHY_API_KEY': JSON.stringify(process.env.GIPHY_API_KEY),
+        'process.env.NODE_ENV': JSON.stringify(configProd ? 'production' : 'development'),
+      }),
+      resolve({ browser: true }),
+      typescript(),
       commonjs({
         include: ['node_modules/**'],
       }),
-      replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') }),
-      resolve({ browser: true }),
-      configServe && serve(),
       configProd && terser(),
-      typescript(),
+      configServe && serve(),
     ],
-    watch: {
-      include: ['src/**'],
-    },
   },
 ];
